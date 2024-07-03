2e03c9367c0e2700d9e094136e49cae79339003c
Jose Miguel Acuña Hernandez <jacunah@unal.edu.co>
2024-07-02 21:15:34 -0500

    un commit

---
+++ Cur_M_L_SKlearn/src/models/models.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import GridSearchCV
+
+from src.utils.utils import Utils
+
--- Cur_M_L_SKlearn/src/utils/utils.py
+++ Cur_M_L_SKlearn/src/utils/utils.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import joblib
 
 class Utils:
 
     def load_from_csv(self, path):
         return pd.read_csv(path)
     
-    def features_target_split(self, df, target_col):
-        X = df.drop(target_col, axis=1)
-        y = df[target_col]
+    def features_target(self, dataset, drop_cols, y):
+        X = dataset.drop(drop_cols, axis=1)
+        y = dataset[y]
         return X, y
+    
+    def model_export(self, clf, score):
+        pass
