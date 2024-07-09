6b8540f7fe3f13c1548f46f6ee22870b3ff48571
Jose Miguel Acuña Hernandez <jacunah@unal.edu.co>
2024-07-09 11:58:42 -0500

    un commit

--- CurPOO/Concesionario/concesionario.py
+++ CurPOO/Concesionario/concesionario.py
@@ -41,7 +41,7 @@ class concesionario:
         
         if cliente in self.clientes:
             self.inventario.append(vehículo)
-            print(f"El cliente {cliente.nombre} {cliente.apellido} ha comprado el vehículo {vehículo.marca} {vehículo.modelo} {vehículo.año} por {vehículo.precio}")
+            print(f"El cliente {cliente.nombre} {cliente.apellido} nos ha vendido el vehículo {vehículo.marca} {vehículo.modelo} {vehículo.año} por {vehículo.precio}")
         
         else:
             print(f"El cliente {cliente.nombre} {cliente.apellido} no está registrado en el concesionario, por favor añadalo primero")
@@ -51,9 +51,41 @@ class concesionario:
         
         if cliente in self.clientes:
             self.inventario.remove(vehículo)
-            print(f"El cliente {cliente.nombre} {cliente.apellido} ha vendido el vehículo {vehículo.marca} {vehículo.modelo} {vehículo.año}")
+            print(f"El cliente {cliente.nombre} {cliente.apellido} ha comprado el vehículo {vehículo.marca} {vehículo.modelo} {vehículo.año}")
 
         else:
             print(f"El cliente {cliente.nombre} {cliente.apellido} no está registrado en el concesionario, por favor añadalo primero")
 
 
+#Creando algunos clientes
+cliente1 = cliente("Juan", "Pérez", "12345678A", "666777888")
+cliente2 = cliente("María", "Gómez", "87654321B", "666777999")
+cliente3 = cliente("Pedro", "Martínez", "12348765C", "666777000")
+
+#Creando algunos vehículos
+vehículo1 = vehículo("Audi", "A3", 2018, 20000, 50000, "Rojo", "Gasolina")
+vehículo2 = vehículo("BMW", "Serie 3", 2019, 25000, 40000, "Azul", "Diésel")
+vehículo3 = vehículo("Mercedes", "Clase A", 2017, 18000, 60000, "Blanco", "Gasolina")
+vehículo4 = vehículo("BYD", "Song Plus", 2024, 50000, 10000, "Gris", "Híbrido")
+
+#Creando el concesionario
+concesionario = concesionario()
+
+#Añadiendo clientes al concesionario
+concesionario.añadir_cliente(cliente1)
+concesionario.añadir_cliente(cliente2)
+concesionario.añadir_cliente(cliente3)
+
+#Inventario del concesionario
+concesionario.inventario.append(vehículo1)
+concesionario.inventario.append(vehículo2)
+
+#Compra de vehículos
+concesionario.comprar_vehiculo(vehículo3, cliente1)
+concesionario.comprar_vehiculo(vehículo4, cliente2)
+
+#Venta de vehículos
+concesionario.venta_vehículo(vehículo1, cliente1)
+concesionario.venta_vehículo(vehículo2, cliente3)
+
+cliente3.consulta_inventario(concesionario)
